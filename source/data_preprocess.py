8e0898da530558c5cede0a075f76149113c7263c
Teslim <olayiwolateslim9@gmail.com>
2023-03-04 15:01:20 -0600

    add ANN implementation

--- source/data_preprocess.py
+++ source/data_preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 class DataPreprocessing():
     def __init__(self):
@@ -10,6 +11,23 @@ class DataPreprocessing():
 
         print(data.head())
 
-        data = data.to_numpy()
+        self.data = data.to_numpy()
 
-        return data
+        return self.data
+
+    def split_data(self, data):
+        train_validation, test = train_test_split(data, test_size = 0.2, random_state=12)
+        
+        return train_validation, test
+    
+    def normalize_data(self, xtrain_data, xtest_data):
+
+        scaler = StandardScaler()
+
+        scaler.fit(xtrain_data)
+
+        norm_xtrain_data = scaler.transform(xtrain_data)
+        norm_xtest_data = scaler.transform(xtest_data)
+        
+        return norm_xtrain_data, norm_xtest_data
+        
--- source/model_builder.py
+++ source/model_builder.py
@@ -1,11 +1,15 @@
 
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
 import numpy as np
+import warnings
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 
 
 from source.data_preprocess import DataPreprocessing
-
+warnings.filterwarnings("ignore")
 
 
 class ModelBuilder(DataPreprocessing):
@@ -14,7 +18,7 @@ class ModelBuilder(DataPreprocessing):
 
     def dt(self, X_train, X_test, y_train, y_test):
         #Create DT model
-        DT_classifier = DecisionTreeClassifier()
+        DT_classifier = MLPClassifier()
 
         #Train the model
         DT_classifier.fit(X_train, y_train)
@@ -32,3 +36,46 @@ class ModelBuilder(DataPreprocessing):
         self.accuracy = accuracy_score(y_test, DT_predicted)
 
         return DT_classifier
+    
+    def ann(self, X_train, X_test, y_train, y_test, hidden_lay_sizes, learn_rate_init, iteration):
+
+        ## params: Create a new jupyter notebook 
+        # to try different values for the hidden_layer_sizes, learning_rate_init, and max_iter hyperparameters (
+
+        # Create ANN model
+        if (hidden_lay_sizes == None) and (learn_rate_init == None) and (iteration == None):
+                
+            ann_classifier = MLPClassifier()
+                
+        else:
+            if (hidden_lay_sizes != None):
+                    
+                ann_classifier = MLPClassifier(
+                                                hidden_layer_sizes = hidden_lay_sizes
+                                               )
+            if (learn_rate_init != None):
+                    
+                ann_classifier = MLPClassifier(
+                                                learning_rate_init = learn_rate_init
+                                               )
+                
+            if (iteration != None):
+                ann_classifier = MLPClassifier(
+                                               max_iter = iteration
+                                               )
+                
+
+        # Train the model
+        ann_classifier.fit(X_train, y_train)
+
+        # Test the model
+        ytrain_predicted = ann_classifier.predict(X_train)
+        ytest_predicted = ann_classifier.predict(X_test)
+
+        # get performance
+        self.trainScores = accuracy_score(y_train, ytrain_predicted)
+        self.testScores = accuracy_score(y_test, ytest_predicted)
+
+        return ann_classifier 
+    
+    
